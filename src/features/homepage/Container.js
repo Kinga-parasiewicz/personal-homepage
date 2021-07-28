e02ba7477ae44b3c022e89ccf153f3481fda7f5a
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-07-28 10:00:10 +0200

    created component Homepage and added styles to Container

---
+++ src/features/homepage/Container.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  min-height:100vh;
+  max-width:1248px;
+  margin:auto;
+  padding:16px;
+  `;
---
+++ src/features/homepage/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Container } from "./Container";
+
+export const Homepage = () => 
+<Container>
+
+</Container>;
