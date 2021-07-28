3eb4d742ab4b52311278d3baea8bef26056643b8
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-07-28 14:52:00 +0200

    created skillsData.js where are the arrays with skills

---
+++ src/features/homepage/skillsData.js
@@ -0,0 +1,30 @@
+export const skills = [
+    "Semantic & accessible HTML",
+    "BEM convention",
+    "Responsive Web Design",
+    "CSS Grid",
+    "CSS Flexbox",
+    "SASS",
+    "JavaScript ES6+",
+    "React",
+    "Redux-Saga",
+    "Redux (Toolkit)",
+    "React Hooks",
+    "React Router",
+    "Working with API (fetch, axios)",
+    "Error handling",
+    "Promises, Async/Await",
+    "NPM",
+    "Git",
+    "Teamwork",
+  ];
+
+  export const nextSkills = [
+    "React Context",
+    "Classes",
+    "Unit testing",
+    "React Native",
+    "Typescript",
+    "Node.js",
+  ];
+  
