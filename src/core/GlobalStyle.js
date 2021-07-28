8675540933a8f188ffc4f1d28db06e565c560a2c
Kinga Parasiewicz <Kingaparasiewicz1997@gmail.com>
2021-07-28 09:58:56 +0200

    created and added GlobalStyle to index.js

---
+++ src/core/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+  box-sizing: border-box;
+  }
+
+  *, ::before, ::after {
+  box-sizing: inherit;
+  }
+  
+  body {
+      background-color: ${({ theme }) => theme.color.whiteLilac};
+      word-break:break-word;
+      overflow-y:scroll;
+      font-size:18px;
+      letter-spacing:0.05em;
+      font-family: 'Inter', sans-serif;
+  }
+`;
